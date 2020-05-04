f21649e845a3948bbf17d206516af12255923c21
Fallen_Breath <yqx791125833@gmail.com>
2020-05-05 00:16:19 +0800

    don't spam doc

--- make_release.bat
+++ make_release.bat
@@ -17,7 +17,7 @@ cd %name%-%ver%
 rd /S /Q .git __pycache__ build
 rm -f %name%.spec .gitignore make_release.bat
 mkdir server
-cp doc\* .
+cp doc\readme_cn.md .
 rm -f plugins\*
 rd /S /Q doc
 cd ..
