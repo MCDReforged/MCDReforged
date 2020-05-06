38b93b25149cf8a3c3b277278eea27a939779183
Fallen_Breath <yqx791125833@gmail.com>
2020-05-06 19:47:57 +0800

    more restriction check

--- MCDReforged.py
+++ MCDReforged.py
@@ -1,22 +1,30 @@
 # -*- coding: utf-8 -*-
 import traceback
 import sys
+import os
+
 
 try:
-	if sys.version_info.major < 3:
+	if sys.version_info.major + sys.version_info.minor * 0.1 < 3.6:
 		print('Python 3.6+ is needed')
 		raise Exception('Python is too old')
+
+	if not os.path.isdir('utils'):
+		print('Cannot found necessary folder "utils", seems that you have launched it in a wrong directory')
+		print('Current working directory: {}'.format(os.getcwd()))
+		raise Exception('Path not right')
+
 	try:
-		from utils.server import Server
 		from utils import constant
+		from utils.server import Server
 	except ModuleNotFoundError:
 		print('It seems that you have not installed all require modules')
 		raise
 
 	if __name__ == '__main__':
-		print('{} {} is starting up'.format(constant.NAME_SHORT, constant.VERSION))
-		print('{} is open source, u can find it here: {}'.format(constant.NAME_SHORT, constant.GITHUB_URL))
-		print('{} is still in development, it may not work well'.format(constant.NAME_SHORT))
+		print('{} {} is starting up'.format(constant.NAME, constant.VERSION))
+		print('{} is open source, u can find it here: {}'.format(constant.NAME, constant.GITHUB_URL))
+		print('{} is still in development, it may not work well'.format(constant.NAME))
 		try:
 			server = Server()
 		except Exception as e:
@@ -24,6 +32,7 @@ try:
 			raise
 		else:
 			server.start()
+
 except:
-	print(traceback.format_exc())
+	traceback.print_exc()
 	input('Exception occurred, press Enter to exit')
