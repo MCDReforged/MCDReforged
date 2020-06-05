eab69ef84725daceb3469a50eea51c9f4a5e0c31
Fallen_Breath <yqx791125833@gmail.com>
2020-06-05 12:03:05 +0800

    move commands to `on_user_info` and more
    - added port detection sample in `on_info`
    - added 3 second delay in `on_mcdr_stop` for testing

--- plugins/sample_plugin.py
+++ plugins/sample_plugin.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 
 import random
+import re
+import time
 from utils.rtext import *
 
 counter = 0
@@ -41,121 +43,119 @@ def on_unload(server):
 
 def on_user_info(server, info):
 	if info.content == 'ping':
-		server.reply(info, 'pong from on_user_info')
+		server.reply(info, 'pong')
 
+	if server.get_permission_level(info) == 3:
+		if info.content == '!!!start':
+			server.start()
 
-def on_info(server, info):
-	if info.is_user:
+		if info.content == '!!!stop':
+			server.stop()
 
-		if info.content == 'ping':
-			server.reply(info, 'pong')
+		if info.content == '!!!stop_exit':
+			server.stop_exit()
 
-		if server.get_permission_level(info) == 3:
-			if info.content == '!!!start':
-				server.start()
+		if info.content == '!!!restart':
+			server.restart()
 
-			if info.content == '!!!stop':
-				server.stop()
+		if info.content == '!!!exit':
+			server.reply(info, 'success: {}'.format(server.exit()))
 
-			if info.content == '!!!stop_exit':
-				server.stop_exit()
+	if info.source == 1 and info.content.startswith('!!!say '):
+		server.say(info.content[6:])
 
-			if info.content == '!!!restart':
-				server.restart()
+	if info.content == '!!!rcon':
+		server.reply(info, 'rcon is running? ' + str(server.is_rcon_running()))
+		if server.is_rcon_running():
+			server.reply(info, '"time query gametime" command result: ' + server.rcon_query('time query gametime'))
 
-			if info.content == '!!!exit':
-				server.reply(info, 'success: {}'.format(server.exit()))
+	if info.content == '!!!permission':
+		server.reply(info, 'Your permission level is {}'.format(server.get_permission_level(info)))
 
-		if info.source == 1 and info.content.startswith('!!!say '):
-			server.say(info.content[6:])
+	if info.content == '!!!error':
+		x = 1 / 0
 
-		if info.content == '!!!rcon':
-			server.reply(info, 'rcon is running? ' + str(server.is_rcon_running()))
-			if server.is_rcon_running():
-				server.reply(info, '"time query gametime" command result: ' + server.rcon_query('time query gametime'))
-
-		if info.content == '!!!permission':
-			server.reply(info, 'Your permission level is {}'.format(server.get_permission_level(info)))
-
-		if info.content == '!!!error':
-			x = 1 / 0
-
-		if info.content == '!!!status':
-			server.reply(info, '''
+	if info.content == '!!!status':
+		server.reply(info, '''
 is_server_running: {}
 is_server_startup: {}
 is_rcon_running: {}
-			'''.strip().format(
-				server.is_server_running(),
-				server.is_server_startup(),
-				server.is_rcon_running(),
-			))
+		'''.strip().format(
+			server.is_server_running(),
+			server.is_server_startup(),
+			server.is_rcon_running(),
+		))
 
-		if info.content == '!!!secret':
-			global secret
-			server.reply(info, 'My secret number is {}\nAnd You know it too {}'.format(
-				secret, server.get_plugin_instance('sample_plugin').secret)
+	if info.content == '!!!secret':
+		global secret
+		server.reply(info, 'My secret number is {}\nAnd You know it too {}'.format(
+			secret, server.get_plugin_instance('sample_plugin').secret)
+		)
+
+	if info.content == '!!!plugin':
+		name = server.get_plugin_list()[0]
+		server.reply(info, 'I found "{}"'.format(name))
+		server.disable_plugin(name)
+		server.reply(info, 'I disabled "{}"'.format(name))
+		server.enable_plugin(name)
+		server.reply(info, 'I enabled "{}"'.format(name))
+		server.load_plugin(name)
+		server.reply(info, 'I reloaded "{}"'.format(name))
+		server.refresh_changed_plugins()
+		server.reply(info, 'I refreshed all changed plugins')
+		server.refresh_all_plugins()
+		server.reply(info, 'I refreshed EVERYTHING!')
+
+	if info.content == '!!!rtext':
+		server.reply(info, RText('RText!!', color=RColor.gold))
+		server.reply(
+			info,
+			RText('hover! ', color=RColor.aqua).set_hover_text('~~~') +
+			RText('click!', styles=RStyle.underlined).set_click_event(RAction.suggest_command, 'yes!')
+		)
+		server.reply(
+			info,
+			RText('More Test', color=RColor.light_purple, styles=[RStyle.italic, RStyle.underlined]).h('QwQ') +
+			'\nMinecraft §aC§bo§cl§do§er §r§lCode\n' +
+			RTextList(
+				RText('>>>>>>> Click me <<<<<<<\n').c(RAction.suggest_command, '!!!RText').h(
+					RText('www', styles=[RStyle.obfuscated, RStyle.underlined]),
+					'<- guess what is this\n',
+					'tbh idk'
+				),
+				RText('Have you clicked§f that?', styles=RStyle.bold).h('stop lazy')
 			)
+		)
 
-		if info.content == '!!!plugin':
-			name = server.get_plugin_list()[0]
-			server.reply(info, 'I found "{}"'.format(name))
-			server.disable_plugin(name)
-			server.reply(info, 'I disabled "{}"'.format(name))
-			server.enable_plugin(name)
-			server.reply(info, 'I enabled "{}"'.format(name))
-			server.load_plugin(name)
-			server.reply(info, 'I reloaded "{}"'.format(name))
-			server.refresh_changed_plugins()
-			server.reply(info, 'I refreshed all changed plugins')
-			server.refresh_all_plugins()
-			server.reply(info, 'I refreshed EVERYTHING!')
+	if info.content == '!!!color':
+		text = '''
+		§0black
+		§1dark_blue
+		§2dark_green
+		§3dark_aqua
+		§4dark_red
+		§5dark_purple
+		§6gold
+		§7gray
+		§8dark_gray
+		§9blue
+		§agreen
+		§baqua
+		§cred
+		§dlight_purple
+		§eyellow
+		§fwhite
+		§lbold
+		§r§krandom (won't work)
+		'''.strip()
+		text = '\n'.join([line.strip() for line in text.splitlines()])
+		server.reply(info, text)
+		server.logger.warning(text)
 
-		if info.content == '!!!rtext':
-			server.reply(info, RText('RText!!', color=RColor.gold))
-			server.reply(
-				info,
-				RText('hover! ', color=RColor.aqua).set_hover_text('~~~') +
-				RText('click!', styles=RStyle.underlined).set_click_event(RAction.suggest_command, 'yes!')
-			)
-			server.reply(
-				info,
-				RText('More Test', color=RColor.light_purple, styles=[RStyle.italic, RStyle.underlined]).h('QwQ') +
-				'\nMinecraft §aC§bo§cl§do§er §r§lCode\n' +
-				RTextList(
-					RText('>>>>>>> Click me <<<<<<<\n').c(RAction.suggest_command, '!!!RText').h(
-						RText('www', styles=[RStyle.obfuscated, RStyle.underlined]),
-						'<- guess what is this\n',
-						'tbh idk'
-					),
-					RText('Have you clicked§f that?', styles=RStyle.bold).h('stop lazy')
-				)
-			)
 
-		if info.content == '!!!color':
-			text = '''
-			§0black
-			§1dark_blue
-			§2dark_green
-			§3dark_aqua
-			§4dark_red
-			§5dark_purple
-			§6gold
-			§7gray
-			§8dark_gray
-			§9blue
-			§agreen
-			§baqua
-			§cred
-			§dlight_purple
-			§eyellow
-			§fwhite
-			§lbold
-			§r§krandom (won't work)
-			'''.strip()
-			text = '\n'.join([line.strip() for line in text.splitlines()])
-			server.reply(info, text)
-			server.logger.warning(text)
+def on_info(server, info):
+	if not info.is_user and re.fullmatch(r'Starting Minecraft server on \S*', info.content):
+		server.reply(info, 'The server bound port {}'.format(info.content.split(':')[-1]))
 
 
 def on_player_joined(server, player):
@@ -184,4 +184,7 @@ def on_server_stop(server, return_code):
 
 
 def on_mcdr_stop(server):
+	server.logger.info('Give me 3 second to prepare exiting')
+	for i in range(3):
+		time.sleep(1)
 	server.logger.info('See you next time~')
