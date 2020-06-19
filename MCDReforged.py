77d3cb561eb6b57c184937f45207f050d36a7c97
Fallen_Breath <yqx791125833@gmail.com>
2020-06-19 22:04:17 +0800

    a fail try of multi process

--- MCDReforged.py
+++ MCDReforged.py
@@ -1,46 +1,91 @@
 # -*- coding: utf-8 -*-
-
 import traceback
 import sys
 import os
+from multiprocessing import set_start_method, Process, Pipe
+from multiprocessing.connection import Connection
+from subprocess import Popen
 
 
-def main():
-	print('{} {} is starting up'.format(constant.NAME, constant.VERSION))
-	print('{} is open source, u can find it here: {}'.format(constant.NAME, constant.GITHUB_URL))
-	print('{} is still in development, it may not work well'.format(constant.NAME))
-	try:
-		server = Server()
-	except:
-		print('Fail to initialize {}'.format(constant.NAME_SHORT))
-		raise
-	else:
-		server.start()
-
-
-try:
-	python_version = sys.version_info.major + sys.version_info.minor * 0.1
-	if python_version < 3.6:
-		print('Python 3.6+ is needed')
-		raise Exception('Python version {} is too old'.format(python_version))
-
-	if not os.path.isfile(os.path.join('utils', '__init__.py')):
-		print('Cannot found necessary module folder "utils", seems that you have launched it in a wrong directory')
-		print('Current working directory: {}'.format(os.getcwd()))
-		raise Exception('Wrong working directory')
-
+def run(child_conn: Connection, fileno):
+	sys.stdin = os.fdopen(fileno)
 	try:
 		from utils import constant
 		from utils.server import Server
+		from utils.server_status import ExitType
 	except ModuleNotFoundError:
 		print('It seems that you have not installed all require modules')
 		raise
 
-	if __name__ == '__main__':
+	print('{} {} is starting up'.format(constant.NAME, constant.VERSION))
+	print('{} is open source, u can find it here: {}'.format(constant.NAME, constant.GITHUB_URL))
+	print('{} is still in development, it may not work well'.format(constant.NAME))
+	server_process = child_conn.recv()  # type: Popen
+	return_data = None
+	try:
+		server = Server()
+	except:
+		print('Fail to initialize {}'.format(constant.NAME_SHORT))
+		traceback.print_exc()
+		if type(server_process) is Popen:
+			while True:
+				try:
+					choice = input('Retry starting up (y) or kill the server process and exit (n)?\n(y/n): ').lower()
+				except (KeyboardInterrupt, EOFError, SystemExit):
+					choice = 'n'
+					break
+				else:
+					if choice in ['y', 'n']:
+						break
+			if choice == 'y':
+				return_data = server_process
+			else:
+				server_process.kill()
+	else:
+		server.start(server_process)
+		if server.exit_type in [ExitType.RESTART]:
+			return_data = server.process
+	finally:
+		try:
+			child_conn.send(return_data)
+		except:
+			if type(return_data) is Popen:
+				return_data.kill()
+			child_conn.send(None)
+			raise
+
+
+def main():
+	set_start_method('spawn')
+	server_process = None
+	while True:
+		parent_conn, child_conn = Pipe()
+		p = Process(target=run, args=(child_conn, sys.stdin.fileno()))
+		p.daemon = True
+		parent_conn.send(server_process)
+		p.start()
+		p.join()
+		server_process = parent_conn.recv()
+		if server_process is None:
+			break
+		print()
+
+
+if __name__ == '__main__':
+	try:
+		python_version = sys.version_info.major + sys.version_info.minor * 0.1
+		if python_version < 3.6:
+			print('Python 3.6+ is needed')
+			raise Exception('Python version {} is too old'.format(python_version))
+
+		if not os.path.isfile(os.path.join('utils', '__init__.py')):
+			print('Cannot found necessary module folder "utils", seems that you have launched it in a wrong directory')
+			print('Current working directory: {}'.format(os.getcwd()))
+			raise Exception('Wrong working directory')
+
 		main()
-except:
-	if __name__ == '__main__':
+
+	except:
 		traceback.print_exc()
 		input('Exception occurred, press Enter to exit')
-	else:
-		raise
+		exit(1)
--- utils/server.py
+++ utils/server.py
@@ -11,23 +11,25 @@ from threading import Lock
 from utils import config, constant, logger, tool
 from utils.exception import *
 from utils.command_manager import CommandManager
+from utils.info import Info
 from utils.parser_manager import ParserManager
 from utils.permission_manager import PermissionManager
 from utils.plugin_manager import PluginManager
 from utils.rcon_manager import RconManager
 from utils.language_manager import LanguageManager
-from utils.server_status import ServerStatus
+from utils.server_status import *
 from utils.server_interface import ServerInterface
 from utils.update_helper import UpdateHelper
 
 
 class Server:
-	def __init__(self, old_process=None):
+	def __init__(self):
 		self.console_input_thread = None
 		self.info_reactor_thread = None
 		self.info_queue = queue.Queue(maxsize=constant.MAX_INFO_QUEUE_SIZE)
-		self.process = old_process  # the process for the server
+		self.process = None  # the process for the server
 		self.server_status = ServerStatus.STOPPED
+		self.exit_type = ExitType.EXIT
 		self.flag_interrupt = False  # ctrl-c flag
 		self.flag_rcon_startup = False  # set to True after server startup. used to start the rcon server
 		self.flag_exit = False  # MCDR exit flag
@@ -110,6 +112,10 @@ class Server:
 		self.server_status = status
 		self.logger.debug('Server status has set to "{}"'.format(status))
 
+	def set_exit_type(self, exit_type):
+		self.exit_type = exit_type
+		self.logger.debug('MCDR exit type has set to "{}"'.format(exit_type))
+
 	def start_server(self) -> bool:
 		"""
 		try to start the server process
@@ -133,32 +139,39 @@ class Server:
 				self.logger.exception(self.t('server.start_server.start_fail'))
 				return False
 			else:
-				self.set_server_status(ServerStatus.RUNNING)
 				self.logger.info(self.t('server.start_server.pid_info', self.process.pid))
 				return True
 		finally:
 			self.starting_server_lock.release()
 
-	def start(self):
+	def start(self, process=None):
 		"""
 		Try to start the server. if succeeded the console thread will start and MCDR will start ticking
 
-		:raise: Raise ServerStartError if the server is already running or start_server has been called by other
+		:param Popen process: The server process object from the previous MCDR instance
+		:raises: Raise ServerStartError if the server is already running or start_server has been called by other. Raise
+		TypeError if the type of the parameter process is invalid
 		"""
 		def start_thread(target, args, name):
 			self.logger.debug('{} thread starting'.format(name))
 			thread = tool.start_thread(target, args, name)
 			return thread
 
-		if not self.start_server():
-			raise ServerStartError()
+		if process is not None:
+			if type(process) is Popen:
+				self.process = process
+			else:
+				raise TypeError('The type of process parameter needs to be Popen, but {} found'.format(type(process).__name__))
+		else:
+			if not self.start_server():
+				raise ServerStartError()
+		self.set_server_status(ServerStatus.RUNNING)
 		if not self.config['disable_console_thread']:
 			self.console_input_thread = start_thread(self.console_input, (), 'Console')
 		else:
 			self.logger.info('Console thread disabled')
 		self.info_reactor_thread = start_thread(self.info_react, (), 'InfoReactor')
 		self.run()
-		return self.process
 
 	def stop(self, forced=False, new_server_status=ServerStatus.STOPPING_BY_ITSELF):
 		"""
@@ -274,23 +287,25 @@ class Server:
 		self.put_info(parsed_result)
 
 	def on_mcdr_stop(self):
-		try:
+		if self.exit_type in [ExitType.EXIT]:
 			if self.flag_interrupt:
 				self.logger.info(self.t('server.on_mcdr_stop.user_interrupted'))
 			else:
 				self.logger.info(self.t('server.on_mcdr_stop.server_stop'))
+		try:
 			self.plugin_manager.call('on_mcdr_stop', (self.server_interface,), wait=True)
-			self.flag_exit = True
-			self.logger.info(self.t('server.on_mcdr_stop.bye'))
 		except:
 			self.logger.exception(self.t('server.on_mcdr_stop.stop_error'))
+		finally:
+			self.flag_exit = True
+			self.logger.info(self.t('server.on_mcdr_stop.bye'))
 
 	def run(self):
 		"""
 		the main loop of MCDR
 		:return: None
 		"""
-		while self.server_status not in [ServerStatus.STOPPED]:
+		while self.server_status not in [ServerStatus.STOPPED] and self.exit_type not in [ExitType.RESTART]:
 			try:
 				if self.is_server_running():
 					self.tick()
@@ -331,6 +346,11 @@ class Server:
 				self.logger.exception(self.t('server.console_input.error'))
 
 	def put_info(self, info):
+		"""
+		Put an Info into info_queue for future processing
+
+		:type info: Info
+		"""
 		try:
 			self.info_queue.put_nowait(info)
 		except queue.Full:
