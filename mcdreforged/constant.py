c8c9309f53ecd20e143f4acb2fda44706187f996
Fallen_Breath <yqx791125833@gmail.com>
2021-01-12 01:11:10 +0800

    1.0.0-alpha.6

--- mcdreforged/constant.py
+++ mcdreforged/constant.py
@@ -8,8 +8,8 @@ import os
 NAME_SHORT = 'MCDR'
 NAME = 'MCDReforged'
 PACKAGE_NAME = 'mcdreforged'
-VERSION = '1.0.0-alpha.5'
-VERSION_PYPI = '1.0.0a5'
+VERSION = '1.0.0-alpha.6'
+VERSION_PYPI = '1.0.0a6'
 GITHUB_URL = 'https://github.com/Fallen-Breath/MCDReforged'
 GITHUB_API_LATEST = 'https://api.github.com/repos/Fallen-Breath/MCDReforged/releases/latest'
 
