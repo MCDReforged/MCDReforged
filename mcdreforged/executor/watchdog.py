201e002e6385336f670cdada64b636d4db149056
Fallen_Breath <yqx791125833@gmail.com>
2021-01-15 13:32:40 +0800

    translation in watchdog

--- mcdreforged/executor/watchdog.py
+++ mcdreforged/executor/watchdog.py
@@ -9,11 +9,12 @@ class WatchDog(ThreadExecutor):
 
 	def check_task_executor_state(self):
 		task_executor = self.mcdr_server.task_executor
-		logger = self.mcdr_server.logger
 		if task_executor.get_this_tick_time() > self.TASK_EXECUTOR_NO_RESPOND_THRESHOLD:
-			logger.warning('Task executor has no respond for {} seconds, something might go wrong'.format(self.TASK_EXECUTOR_NO_RESPOND_THRESHOLD))
-			logger.warning('Current running plugin (if there\'s any): {}'.format(self.mcdr_server.plugin_manager.get_current_running_plugin(thread=task_executor.get_thread())))
-			logger.warning('Recreating the task executor')
+			plugin = self.mcdr_server.plugin_manager.get_current_running_plugin(thread=task_executor.get_thread())
+			self.mcdr_server.logger.warning(self.mcdr_server.tr('watchdog.task_executor_no_response.line1', self.TASK_EXECUTOR_NO_RESPOND_THRESHOLD))
+			self.mcdr_server.logger.warning(self.mcdr_server.tr('watchdog.task_executor_no_response.line2', plugin))
+			self.mcdr_server.logger.warning(self.mcdr_server.tr('watchdog.task_executor_no_response.line3'))
+
 			task_executor.soft_stop()  # Soft-stop it, in case it turns alive somehow
 			task_executor.set_name(task_executor.get_name() + ' (no response)')
 
