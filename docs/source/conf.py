aa1d413bb13907b0ba3a76d959b8ee617b498bb5
Fallen_Breath <yqx791125833@gmail.com>
2021-08-01 01:41:33 +0800

    v1.6.0-alpha.1

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -23,7 +23,7 @@ copyright = '2021, Fallen_Breath'
 author = 'Fallen_Breath'
 
 # The full version, including alpha/beta/rc tags
-release = '1.5'
+release = '1.6'
 
 
 # -- General configuration ---------------------------------------------------
