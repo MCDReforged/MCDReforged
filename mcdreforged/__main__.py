aeda4c5b457d70ab6022be96404ec843ff3ecd17
Huaji_MUR233 <47046832+HuajiMUR233@users.noreply.github.com>
2021-08-08 22:04:31 +0800

    try

--- mcdreforged/__main__.py
+++ mcdreforged/__main__.py
@@ -3,11 +3,14 @@ Entry for MCDR
 """
 import sys
 
+from prompt_toolkit.patch_stdout import patch_stdout
+
 from mcdreforged.cli import cli_entry
 
 
 def main():
-	return cli_entry.entry_point()
+	with patch_stdout(raw=True):
+		return cli_entry.entry_point()
 
 
 if __name__ == '__main__':
