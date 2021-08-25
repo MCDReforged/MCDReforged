aaaaf0710d75eb2bb672c86153ff565a004a2e1d
Fallen_Breath <yqx791125833@gmail.com>
2021-08-26 01:13:20 +0800

    v2.0.5

--- mcdreforged/constants/core_constant.py
+++ mcdreforged/constants/core_constant.py
@@ -11,8 +11,8 @@ PACKAGE_NAME = 'mcdreforged'
 
 # MCDR Version Storage
 # Related: docs/source/conf.py
-VERSION = '2.0.4'       # semver (1.2.3-alpha.4)
-VERSION_PYPI = '2.0.4'  # pythonic ver (1.2.3a4)
+VERSION = '2.0.5'       # semver (1.2.3-alpha.4)
+VERSION_PYPI = '2.0.5'  # pythonic ver (1.2.3a4)
 
 GITHUB_URL = 'https://github.com/Fallen-Breath/MCDReforged'
 GITHUB_API_LATEST = 'https://api.github.com/repos/Fallen-Breath/MCDReforged/releases/latest'
