6fd54bede6c3102fb9341d862d463d2113d1ab71
Fallen_Breath <yqx791125833@gmail.com>
2021-09-03 17:36:29 +0800

    fixed FileNotFoundError in `save_config_simple` if the path is a plain file

--- mcdreforged/plugin/server_interface.py
+++ mcdreforged/plugin/server_interface.py
@@ -759,7 +759,7 @@ class PluginServerInterface(ServerInterface):
 		else:
 			data = config
 		target_folder = os.path.dirname(config_file_path)
-		if not os.path.isdir(target_folder):
+		if len(target_folder) > 0 and not os.path.isdir(target_folder):
 			os.makedirs(target_folder)
 		with open(config_file_path, 'w') as file:
 			json.dump(data, file, indent=4, ensure_ascii=False)
