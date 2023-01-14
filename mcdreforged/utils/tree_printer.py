274c88f6cefeeb81aa4ec56a002cf003d3a703a1
Fallen_Breath <yqx791125833@gmail.com>
2023-01-14 17:43:29 +0800

    builder like TreePrinter class

--- mcdreforged/utils/tree_printer.py
+++ mcdreforged/utils/tree_printer.py
@@ -1,47 +1,82 @@
-from typing import Callable, Any, TypeVar, Iterable, Sized
+from typing import Callable, Any, TypeVar, Iterable, Sized, Generic
 
 __all__ = [
 	'LineWriter',
+	'TreePrinter',
 	'print_tree',
 ]
 
 
-LineWriter = Callable[[str], Any]
-
-
 T = TypeVar('T')
+Self = TypeVar('Self', bound='TreePrinter')
+LineWriter = Callable[[str], Any]
+_ChildrenGetter = Callable[[T], Iterable[T]]
+_NameGetter = Callable[[T], str]
 
 
-def print_tree(
-		root: T, children_getter: Callable[[T], Iterable[T]], name_getter: Callable[[T], str], line_writer: LineWriter, *,
-		use_tab: bool = False
-):
-	def is_root(node: T) -> bool:
-		return node == root
+class TreePrinter(Generic[T]):
+	def __init__(self):
+		self.__line_writer: LineWriter = print
+		self.__children_getter = None
+		self.__name_getter = None
+		self.__use_tab = False
+		self.__use_ascii = False
 
-	def get_item_line(node: T, is_last: bool) -> str:
-		if is_root(node):
-			return ''
-		return '└── ' if is_last else '├── '
+	def getters(self: Self, children_getter: _ChildrenGetter, name_getter: _NameGetter) -> Self:
+		self.__children_getter = children_getter
+		self.__name_getter = name_getter
+		return self
 
-	def get_parent_line(node: T, is_last: bool) -> str:
-		if is_root(node):
-			return ''
-		if use_tab:
-			return '\t' if is_last else '│\t'
-		else:
-			return '    ' if is_last else '│   '
+	def writer(self: Self, line_writer: LineWriter) -> Self:
+		self.__line_writer = line_writer
+		return self
 
-	def do_print(node: T, prefix: str, is_last: bool):
-		line = name_getter(node)
-		line = get_item_line(node, is_last) + line
-		line_writer(prefix + line)
+	def tab(self: Self) -> Self:
+		self.__use_tab = True
+		return self
 
-		children = children_getter(node)
-		if not isinstance(children, Sized):
-			children = tuple(children)
-		for i, child in enumerate(children):
-			do_print(child, prefix + get_parent_line(node, is_last), i == len(children) - 1)
+	def ascii(self: Self) -> Self:
+		self.__use_ascii = True
+		return self
 
-	do_print(root, '', False)
+	def print(self: Self, root: T) -> Self:
+		def is_root(node: T) -> bool:
+			return node == root
 
+		def get_item_line(node: T, is_last: bool) -> str:
+			if is_root(node):
+				return ''
+			if self.__use_ascii:
+				return '`-- ' if is_last else '+-- '
+			else:
+				return '└── ' if is_last else '├── '
+
+		def get_parent_line(node: T, is_last: bool) -> str:
+			if is_root(node):
+				return ''
+			if is_last:
+				base = '' if self.__use_tab else ' '
+			else:
+				base = '|' if self.__use_ascii else '│'
+			padding = '\t' if self.__use_tab else '   '
+			return base + padding
+
+		def do_print(node: T, prefix: str, is_last: bool):
+			line = self.__name_getter(node)
+			line = get_item_line(node, is_last) + line
+			self.__line_writer(prefix + line)
+
+			children = self.__children_getter(node)
+			if not isinstance(children, Sized):
+				children = tuple(children)
+			for i, child in enumerate(children):
+				do_print(child, prefix + get_parent_line(node, is_last), i == len(children) - 1)
+
+		assert self.__children_getter is not None, 'children_getter not assigned'
+		assert self.__name_getter is not None, 'name_getter not assigned'
+		do_print(root, '', False)
+		return self
+
+
+def print_tree(root: T, children_getter: _ChildrenGetter, name_getter: _NameGetter, line_writer: LineWriter):
+	TreePrinter().writer(line_writer).getters(children_getter, name_getter).print(root)
