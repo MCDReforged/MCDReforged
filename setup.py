28296471802a03378e40871c4bea4a7f5e5c6e69
Fallen_Breath <yqx791125833@gmail.com>
2023-05-23 11:57:12 +0800

    support console entrypoint script
    now after installed, command `mcdreforged` is available, as a shortcut of `python -m mcdreforged`

--- setup.py
+++ setup.py
@@ -10,16 +10,22 @@ from mcdreforged.constants import core_constant
 # python -m twine upload --repository testpypi dist/*
 # python -m twine upload dist/*
 
+# -------------------------------- Basics --------------------------------
+
 NAME = core_constant.PACKAGE_NAME
 VERSION = core_constant.VERSION_PYPI
 DESCRIPTION = 'A rewritten version of MCDaemon, a python script to control your Minecraft server'
-URL = 'https://github.com/Fallen-Breath/MCDReforged'
+PROJECT_URLS = {
+	'Homepage': core_constant.GITHUB_URL,
+	'Documentation': core_constant.DOCUMENTATION_URL,
+}
 AUTHOR = 'Fallen_Breath'
-REQUIRES_PYTHON = '>=3.6.0'
+REQUIRES_PYTHON = '>=3.6'
 
 CLASSIFIERS = [
 	# https://pypi.org/classifiers/
 	'License :: OSI Approved :: GNU Lesser General Public License v3 (LGPLv3)',
+	'Operating System :: OS Independent',
 	'Programming Language :: Python',
 	'Programming Language :: Python :: 3',
 	'Programming Language :: Python :: 3.6',
@@ -28,27 +34,35 @@ CLASSIFIERS = [
 	'Programming Language :: Python :: 3.9',
 	'Programming Language :: Python :: 3.10',
 	'Programming Language :: Python :: 3.11',
-	'Operating System :: OS Independent'
 ]
 
+ENTRY_POINTS = {
+	'console_scripts': [
+		'{0} = {0}.cli.cli_entry:entry_point'.format(core_constant.PACKAGE_NAME)
+	]
+}
+print('ENTRY_POINTS = {}'.format(ENTRY_POINTS))
+
+# github action environment
 if os.getenv('CI', None) is not None:
-	build_num = os.getenv('GITHUB_RUN_NUMBER', None)
+	build_num = os.getenv('GITHUB_RUN_NUMBER')
 	is_release = os.getenv('GITHUB_REF', '').startswith('refs/tags/v')
 	if build_num is not None and not is_release:
 		VERSION += '.dev{}'.format(build_num)
 
-# ----------------------------------------------------------------
+# -------------------------------- Files --------------------------------
 
 here = os.path.abspath(os.path.dirname(__file__))
 
 with open(os.path.join(here, 'requirements.txt')) as f:
-	REQUIRED = [line for line in f.readlines() if not len(line.strip()) == 0]
-
-print('REQUIRED = {}'.format(REQUIRED))
+	REQUIRED = list(filter(None, map(str.strip, f)))
+	print('REQUIRED = {}'.format(REQUIRED))
 
 with open(os.path.join(here, 'README.md'), encoding='utf8') as f:
 	LONG_DESCRIPTION = f.read()
 
+
+# -------------------------------- Setup --------------------------------
 
 setup(
 	name=NAME,
@@ -58,10 +72,10 @@ setup(
 	long_description_content_type='text/markdown',
 	author=AUTHOR,
 	python_requires=REQUIRES_PYTHON,
-	url=URL,
+	project_urls=PROJECT_URLS,
 	packages=find_packages(exclude=['tests', '*.tests', '*.tests.*', 'tests.*']),
-	package_data={core_constant.PACKAGE_NAME: ["py.typed"]},
-	install_requires=REQUIRED,
 	include_package_data=True,
+	install_requires=REQUIRED,
 	classifiers=CLASSIFIERS,
+	entry_points=ENTRY_POINTS,
 )
