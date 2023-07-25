MCDReforged/MCDReforged

commit efab0d4ef55d4ccbcf2895e451434ab6355bbc98

updated requirement, fixed ruamel.yaml loading/saving issue with ruamel.yaml~=0.17

mcdreforged/utils/yaml_data_storage.py
@@ -2,7 +2,7 @@ import os
 from logging import Logger
 from threading import RLock
 
-from ruamel import yaml
+from ruamel.yaml import YAML
 from ruamel.yaml.comments import CommentedMap
 
 from mcdreforged.utils import resources_util
@@ -41,7 +41,7 @@ class YamlDataStorage:
 		"""
 		if self.file_presents():
 			with open(self.__file_path, encoding='utf8') as file:
-				users_data = yaml.round_trip_load(file)
+				users_data = YAML().load(file)
 		else:
 			if not allowed_missing_file:
 				raise FileNotFoundError()
@@ -85,7 +85,7 @@ class YamlDataStorage:
 		with self._data_operation_lock:
 			self._pre_save(data)
 			with open(self.__file_path, 'w', encoding='utf8') as file:
-				yaml.round_trip_dump(data, file, width=4096)  # specifying width=4096 to prevent yaml breaks long string into multiple lines
+				YAML().dump(data, file)
 
 	def save(self):
 		self.__save(self._data)

mcdreforged/utils/resources_util.py
@@ -10,7 +10,7 @@ __all__ = [
 	'get_data', 'get_yaml'
 ]
 
-from ruamel import yaml
+from ruamel.yaml import YAML
 from ruamel.yaml.comments import CommentedMap
 
 from mcdreforged.constants import core_constant
@@ -32,7 +32,8 @@ def get_yaml(path: str) -> CommentedMap:
 	bytes_data = get_data(path)
 	# Replace CRLF or yaml loader will load extra lines
 	string_data = bytes_data.decode('utf8').replace('\r\n', '\n')
-	return yaml.round_trip_load(string_data)
+	ret = YAML().load(string_data)
+	return ret
 
 
 if __name__ == '__main__':

commit bef9aa85ea69d7e88d95ddd3544c465a6312c755

fixed long line in yaml file will be wrapped when writing
issue introduced in efab0d4ef55d4ccbcf2895e451434ab6355bbc98

mcdreforged/utils/yaml_data_storage.py
@@ -84,7 +84,9 @@ class YamlDataStorage:
 	def __save(self, data: CommentedMap):
 		self._pre_save(data)
 		with file_util.safe_write(self.__file_path, encoding='utf8') as file:
-			YAML().dump(data, file)
+			yaml = YAML()
+			yaml.width = 1048576  # prevent yaml breaks long string into multiple lines
+			yaml.dump(data, file)
 
 	def save(self):
 		with self._data_operation_lock: