1baf79becbaded429ba204d64af4138958d6c183
Fallen_Breath <yqx791125833@gmail.com>
2023-11-08 16:09:42 +0800

    Make sure current directory is in the `sys.path`
    resolved #277

--- mcdreforged/mcdr_boostrap.py
+++ mcdreforged/mcdr_boostrap.py
@@ -1,4 +1,5 @@
 import io
+import os
 import sys
 import warnings
 
@@ -8,6 +9,7 @@ def boostrap():
 	Do some boostrap things before non-std modules get loaded
 	"""
 
+	# =============================================================================
 	# 1. Ensure line_buffering is True for stdout / stderr
 	# Prevent stdout / stderr goes into block-buffered mode, if the stdio of MCDR is wrapped by other programs
 	# See issue #257
@@ -18,3 +20,17 @@ def boostrap():
 			stream.reconfigure(line_buffering=True)
 		else:
 			warnings.warn('{} {} is not a io.TextIOWrapper, cannot apply reconfigure'.format(name, stream))
+
+	# =============================================================================
+	# 2. Make sure current directory is in the sys.path
+	# Ensure the module search behavior consistency between different launch methods, e.g. "python -m mcdreforged" and "mcdreforged"
+	# Reference: https://docs.python.org/3/library/sys.html#sys.path
+	# See issue #277
+
+	cwd = os.getcwd()
+	path0 = sys.path[0]
+	try:
+		if os.path.isdir(cwd) and os.path.isdir(path0) and not os.path.samefile(cwd, path0):
+			sys.path.insert(0, cwd)
+	except OSError as e:
+		warnings.warn('fail to check equality between os.getcwd() {!r} and sys.path[0] {!r}: {}'.format(cwd, path0, e))
