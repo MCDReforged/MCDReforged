c381d432b8c6820b0d353f72fe8620ef0e007cb3
sstzer <2196814821@qq.com>
2024-09-29 21:35:48 +0800

    fixed environment_check

--- mcdreforged/mcdr_entrypoint.py
+++ mcdreforged/mcdr_entrypoint.py
@@ -12,7 +12,7 @@ def __environment_check():
 	"""
 	This should even work in python 2.7+
 	"""
-	if sys.version_info < (3,8):
+	if sys.version_info < (3, 8):
 		print('Python 3.8+ is needed to run {}'.format(core_constant.NAME))
 		print('Current Python version {} is too old'.format(platform.python_version()))
 		sys.exit(1)
