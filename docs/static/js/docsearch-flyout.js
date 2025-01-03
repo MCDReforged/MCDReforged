8b00fa859375793601a52fa90216f9559e301151
Alex3236 <45303195+alex3236@users.noreply.github.com>
2025-01-03 13:45:13 +0800

    fix: ensure flyout is initialized

--- docs/static/js/docsearch-flyout.js
+++ docs/static/js/docsearch-flyout.js
@@ -1,7 +1,19 @@
+function waitForElement(selector, callback) {
+    if (document.querySelector(selector)) {
+        callback();
+    } else {
+        setTimeout(function() {
+            waitForElement(selector, callback);
+        }, 100);
+    }
+}
+
 window.addEventListener('load', () => {
-    let flyout = document.querySelector('readthedocs-flyout').shadowRoot;
-    let main = flyout.querySelector("main");
-    main.addEventListener('click', (e) => {
-        e.stopPropagation();
-    });
+    waitForElement('readthedocs-flyout', () => {
+        let flyout = document.querySelector('readthedocs-flyout').shadowRoot;
+        let main = flyout.querySelector("main");
+        main.addEventListener('click', (e) => {
+            e.stopPropagation();
+        });
+    })
 })
