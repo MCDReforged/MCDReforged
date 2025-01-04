afd04d181f6e9d2884b330565368a01e1dbed22e
Alex3236 <45303195+alex3236@users.noreply.github.com>
2025-01-04 22:14:06 +0800

    try to use `readthedocs-addons-data-ready`

--- docs/static/js/readthedoc-flyout.js
+++ docs/static/js/readthedoc-flyout.js
@@ -1,19 +1,19 @@
-function waitForElement(selector, callback) {
-    if (document.querySelector(selector)) {
-        callback();
-    } else {
-        setTimeout(function() {
-            waitForElement(selector, callback);
-        }, 100);
-    }
-}
+// function waitForElement(selector, callback) {
+//     if (document.querySelector(selector)) {
+//         callback();
+//     } else {
+//         setTimeout(function() {
+//             waitForElement(selector, callback);
+//         }, 100);
+//     }
+// }
 
-window.addEventListener('load', () => {
-    waitForElement('readthedocs-flyout', () => {
-        let flyout = document.querySelector('readthedocs-flyout').shadowRoot;
-        let target = flyout.querySelector("header");
-        target.addEventListener('click', (e) => {
-            e.stopPropagation();
-        });
-    })
+window.addEventListener('readthedocs-addons-data-ready', () => {
+    // waitForElement('readthedocs-flyout', () => {
+    let flyout = document.querySelector('readthedocs-flyout').shadowRoot;
+    let target = flyout.querySelector("header");
+    target.addEventListener('click', (e) => {
+        e.stopPropagation();
+    });
+    // })
 })
