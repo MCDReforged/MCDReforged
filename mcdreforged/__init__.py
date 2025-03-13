9761d860fc30cf08d0c981385432af749dd6e3b6
Fallen_Breath <nellaforax@outlook.com>
2025-03-14 01:59:46 +0800

    make pycharm happy

--- mcdreforged/__init__.py
+++ mcdreforged/__init__.py
@@ -3,7 +3,7 @@
 	Module ``mcdreforged`` now contains everything in ``mcdreforged.api.all``. All MCDR API components can be imported from here
 """
 
+from mcdreforged.api.all import *
+
 # noinspection PyPep8Naming
 from mcdreforged.constants.core_constant import VERSION_PYPI as __version__
-
-from mcdreforged.api.all import *
