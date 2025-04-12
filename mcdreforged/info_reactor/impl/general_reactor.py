c6d15c401670e710a7177930ea446aefccd50721
Fallen_Breath <nellaforax@outlook.com>
2025-04-12 17:46:04 +0800

    Fixed `Info.cancel_send_to_server` does not work

--- mcdreforged/info_reactor/impl/general_reactor.py
+++ mcdreforged/info_reactor/impl/general_reactor.py
@@ -16,7 +16,10 @@ class GeneralReactor(AbstractInfoReactor):
 		if command_source is not None:
 			self.mcdr_server.command_manager.execute_command(info.content, command_source)
 
-		self.mcdr_server.plugin_manager.dispatch_event(MCDRPluginEvents.GENERAL_INFO, (info,))
+		# The subsequent code flow needs to check the `cancel_send_to_server` status of the `info`,
+		# so the `dispatch_event` calls here needs cannot be delay with `DispatchEventPolicy.always_new_task`
+		dp_ensure_on_thread = self.mcdr_server.plugin_manager.DispatchEventPolicy.ensure_on_thread
+		self.mcdr_server.plugin_manager.dispatch_event(MCDRPluginEvents.GENERAL_INFO, (info,), dispatch_policy=dp_ensure_on_thread)
 
 		if info.is_user:
-			self.mcdr_server.plugin_manager.dispatch_event(MCDRPluginEvents.USER_INFO, (info,))
+			self.mcdr_server.plugin_manager.dispatch_event(MCDRPluginEvents.USER_INFO, (info,), dispatch_policy=dp_ensure_on_thread)
