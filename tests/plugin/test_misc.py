MCDReforged/MCDReforged

commit 9d7278eed43fd49ec0c6bebd9af154690f1548d6

Enhance legality checks for directory and packed plugins (#383)
* refactor: enhance legality checks for directory and packed plugins

* refactor: add check for root `.py`s only.

* style

* fix

* fix

* wierd

* typo

mcdreforged/plugin/type/directory_plugin.py
@@ -26,13 +26,17 @@ class _DirectoryPluginBase(MultiFilePlugin, ABC):
 		return os.listdir(self._file_root / directory_name)
 
 	@override
-	def _check_subdir_legality(self):
-		for package_name in os.listdir(self._file_root):
-			path = self._file_root / package_name
+	def _check_dir_legality(self):
+		plugin_id = self.get_id()
+		for name in os.listdir(self._file_root):
+			path = self._file_root / name
 			if path.is_dir():
 				is_module = (path / '__init__.py').is_file()
-				if is_module and package_name != self.get_id():
-					raise IllegalPluginStructure('Packed plugin cannot contain other package: found package {}'.format(package_name))
+				if is_module and name != plugin_id:
+					raise IllegalPluginStructure('Directory plugin cannot contain other package: found package {}'.format(name))
+			else:
+				if path.suffix == '.py' and path.stem != plugin_id:
+					raise IllegalPluginStructure('Directory plugin cannot contain other module: found module {}'.format(path.stem))
 
 	@override
 	def file_exists(self):

mcdreforged/plugin/type/multi_file_plugin.py
@@ -69,7 +69,7 @@ class MultiFilePlugin(RegularPlugin, ABC):
 		with meta_file:
 			self._set_metadata(Metadata(json.load(meta_file), plugin=self))
 		self.__check_requirements()
-		self._check_subdir_legality()
+		self._check_dir_legality()
 
 	@override
 	def _on_unload(self):
@@ -112,7 +112,7 @@ class MultiFilePlugin(RegularPlugin, ABC):
 			except Exception:
 				self.mcdr_server.logger.exception('Fail to load default translation from file {} in {}'.format(file_path, repr(self)))
 
-	def _check_subdir_legality(self):
+	def _check_dir_legality(self):
 		"""
 		Make sure the only python submodule inside the plugin is named with the plugin id
 		:raise IllegalPluginStructure if check failed

mcdreforged/plugin/type/packed_plugin.py
@@ -65,20 +65,25 @@ class PackedPlugin(MultiFilePlugin):
 		return result
 
 	@override
-	def _check_subdir_legality(self):
+	def _check_dir_legality(self):
+		plugin_id = self.get_id()
 		for file_info in self.__zip_file.infolist():
+			name = file_info.filename.rstrip('/')
+			if '/' in name: # not at root
+				continue
 			if file_info.is_dir():
-				package_name: str = file_info.filename[:-1]  # removing the ending '/'
-				if '/' in package_name:  # not at root
-					continue
 				try:
-					init_info = self.__zip_file.getinfo(os.path.join(package_name, '__init__.py'))
+					init_info = self.__zip_file.getinfo(os.path.join(name, '__init__.py'))
 				except KeyError:
 					is_module = False
 				else:
 					is_module = not init_info.is_dir()
-				if is_module and package_name != self.get_id():
-					raise IllegalPluginStructure('Packed plugin cannot contain other package: found package {}'.format(package_name))
+				if is_module and name != plugin_id:
+					raise IllegalPluginStructure('Packed plugin cannot contain other package: found package {}'.format(name))
+			elif name.endswith('.py'):
+				module_name = name[:-3]  # remove .py
+				if module_name != plugin_id:
+					raise IllegalPluginStructure('Packed plugin cannot contain other module: found module {}'.format(module_name))
 
 	# noinspection PyProtectedMember,PyUnresolvedReferences
 	@override

commit 383b017539d74681b5ed1dac01c060a2e6f93945

fix issue introduced from #383

mcdreforged/plugin/type/directory_plugin.py
@@ -35,7 +35,7 @@ class _DirectoryPluginBase(MultiFilePlugin, ABC):
 				if is_module and name != plugin_id:
 					raise IllegalPluginStructure('Directory plugin cannot contain other package: found package {}'.format(name))
 			else:
-				if path.suffix == '.py' and path.stem != plugin_id:
+				if path.suffix == '.py' and self._ILLEGAL_ROOT_PY_FILE_STEM.fullmatch(path.stem):
 					raise IllegalPluginStructure('Directory plugin cannot contain other module: found module {}'.format(path.stem))
 
 	@override
@@ -104,9 +104,9 @@ class LinkedDirectoryPlugin(_DirectoryPluginBase):
 		}
 
 	@override
-	def _check_subdir_legality(self):
+	def _check_dir_legality(self):
 		if not self.__ldp_meta.skip_package_legality_check:
-			super()._check_subdir_legality()
+			super()._check_dir_legality()
 
 	@override
 	def _on_load(self):

mcdreforged/plugin/type/multi_file_plugin.py
@@ -1,6 +1,7 @@
 import importlib
 import json
 import os
+import re
 import sys
 from abc import ABC, abstractmethod
 from pathlib import Path
@@ -111,6 +112,8 @@ class MultiFilePlugin(RegularPlugin, ABC):
 				self.register_translation(language, translations)
 			except Exception:
 				self.mcdr_server.logger.exception('Fail to load default translation from file {} in {}'.format(file_path, repr(self)))
+
+	_ILLEGAL_ROOT_PY_FILE_STEM = re.compile(r'^[A-Za-z_](?!_)[A-Za-z0-9_]*$')
 
 	def _check_dir_legality(self):
 		"""

mcdreforged/plugin/type/packed_plugin.py
@@ -85,7 +85,7 @@ class PackedPlugin(MultiFilePlugin):
 					raise IllegalPluginStructure('Packed plugin cannot contain other package: found package {}'.format(name))
 			elif name.endswith('.py'):
 				module_name = name[:-3]  # remove .py
-				if module_name != plugin_id:
+				if self._ILLEGAL_ROOT_PY_FILE_STEM.fullmatch(module_name):
 					raise IllegalPluginStructure('Packed plugin cannot contain other module: found module {}'.format(module_name))
 
 	@override

tests/plugin/test_misc.py
@@ -1,0 +1,34 @@
+import unittest
+
+from mcdreforged.plugin.type.multi_file_plugin import MultiFilePlugin
+
+
+class PluginMiscTestCase(unittest.TestCase):
+	def test_multi_file_plugin_check_dir_legality(self):
+		pattern = MultiFilePlugin._ILLEGAL_ROOT_PY_FILE_STEM
+
+		def assert_allowed(s: str):
+			self.assertIsNone(pattern.fullmatch(s), 'assert_allowed() failed, pattern {} does not match {!r} (which should)'.format(pattern, s))
+
+		def assert_illegal(s: str):
+			self.assertIsNotNone(pattern.fullmatch(s), 'assert_illegal() failed, pattern {} matches {!r} (which should not)'.format(pattern, s))
+
+		assert_allowed('__init__')
+		assert_allowed('__main__')
+		assert_allowed('__')
+		assert_allowed('0abc')
+		assert_allowed('Foo-Bar')
+		assert_allowed('Foo+Bar')
+		assert_allowed('.foo')
+		assert_allowed('测试')
+
+		assert_illegal('init')
+		assert_illegal('main')
+		assert_illegal('abc0')
+		assert_illegal('Foo_Bar')
+		assert_illegal('FooBar')
+		assert_illegal('_foo')
+
+
+if __name__ == '__main__':
+	unittest.main()